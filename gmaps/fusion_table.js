7604a665169d9d487144759249542088e02f6322
Amy <amy_yu@mit.edu>
2013-04-27 14:28:41 -0400

    updated main map to show aff or area buckets, transit overlay

--- gmaps/fusion_table.js
+++ gmaps/fusion_table.js
@@ -1,3 +1,5 @@
+var arealayer, afflayer, transitLayer, map;
+
 function initGMap() {
     var boston_latlng = new google.maps.LatLng(42.37, -71.1);
     var mapProp = {
@@ -5,17 +7,62 @@ function initGMap() {
         zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
-    var map = new google.maps.Map(document.getElementById("gmap") ,mapProp);
+    map = new google.maps.Map(document.getElementById("gmap") ,mapProp);
 
-    var layer = new google.maps.FusionTablesLayer({
+    arealayer = new google.maps.FusionTablesLayer({
         query: {
             select: "Latitude",
-            from: "1zy3N82N7aj07_hRhlm3JlDPe8nSnzrQeMg-_vEA",
-        }
+            from: "1Qo9SLRIPdSb0EQ3hZkmhZ3zwFliDZFJBlq-Lr7I",
+        }, 
+        styles:[{
+            markerOptions:{
+                iconName: "small_green",
+                animation: google.maps.Animation.DROP
+            }
+        }]
     });
-    layer.setMap(map);
+
+    afflayer = new google.maps.FusionTablesLayer({
+        query: {
+            select: "Latitude",
+            from: "1eQqFnqJ2QvYRWPNgqrD-ou06vEXHNCZ7YCAD6-4",
+        }, 
+        styles:[{
+            markerOptions:{
+                iconName: "small_red",
+                animation: google.maps.Animation.DROP
+            }
+        }]
+    });
+
+    transitLayer = new google.maps.TransitLayer();
+}
+
+// Hide the current data overlays
+function clearOverlays() {
+    // TODO: clear the form selections
+    arealayer.setMap(null);
+    afflayer.setMap(null);
+    transitLayer.setMap(null);
+}
+
+// Show the selected data overlays
+function showOverlays() {
+    clearOverlays();
+    var cat = $('input[name=category]:checked').val();
+    var transit = $('input[name="transit"]:checked').val();
+    if(cat == "AREA"){
+        arealayer.setMap(map);
+    }
+    else if (cat == "AFFILIATION"){
+        afflayer.setMap(map);   
+    }
+    if(transit == "on"){
+        transitLayer.setMap(map);
+    }
 }
 
 $(document).ready(function() {
     initGMap();
+    showOverlays();
 });

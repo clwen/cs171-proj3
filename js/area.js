ba29bd55ff99796d41ac2c007c861a5b0f209cf9
Chunglin Wen <jonathan.clwen@gmail.com>
2013-05-01 15:11:43 -0400

    remove redundant highlightCommuteMode

--- js/area.js
+++ js/area.js
@@ -225,30 +225,6 @@ var show_number = function() {
     }); // end of d3.csv
 };
 
-var highlightCommuteMode = function(hl) {
-    if (hl === "CLEAR") {
-        modesUsed.forEach( function(m) {
-            var eid = '#' + m;
-            $(eid, "#area-chart").css("fill", comColors[m]);
-        });
-        for (var abbr in abbrToWord){
-            $("#" + abbrToWord[abbr] + " rect")
-            .css("fill", comColors[abbr]);
-        }
-    } else {
-        // gray out all the paths
-        $(".area").css("fill", "#999");
-        // turn on color for selected mode
-        var eid_hl = '#' + hl;
-        $(eid_hl, "#area-chart").css("fill", comColors[hl]);
-        // Highlight the bar chart
-        $("#bar rect")
-            .css("fill", "#999");
-        $("#" + abbrToWord[hl] + " rect")
-            .css("fill", comColors[hl]);
-    }
-};
-
 $(document).ready(function() {
     // toggle between raw number and percentage
     $('input:radio[name=repr]').click(function() {

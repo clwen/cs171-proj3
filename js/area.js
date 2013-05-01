63be3c7172da1505d4a2e3d632c8b8f24594cff2
Chunglin Wen <jonathan.clwen@gmail.com>
2013-04-30 22:23:01 -0400

    fixed legend css bug; removed old mode highliting

--- js/area.js
+++ js/area.js
@@ -267,24 +267,6 @@ $(document).ready(function() {
         }
     });
 
-    // toggle the highlight between different commute modes
-    $('input:radio[name=hl]').click(function() {
-        var hl = $('input:radio[name=hl]:checked').val();
-        // if it's clear, clear the highlight, i.e., all modes back in color
-        if (hl === "CLEAR") {
-            modesUsed.forEach( function(m) {
-                var eid = '#' + m;
-                $(eid, "#area-chart").css("fill", comColors[m]);
-            });
-        } else {
-            // gray out all the paths
-            $(".area").css("fill", "#999");
-            // turn on color for selected mode
-            var eid_hl = '#' + hl;
-            $(eid_hl, "#area-chart").css("fill", comColors[hl]);
-        }
-    });
-
     show_percentage();
 
     // add legend

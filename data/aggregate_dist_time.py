1e4f86ddab52ca3adc32e243d42570e989e0601e
Chunglin Wen <jonathan.clwen@gmail.com>
2013-05-01 17:15:08 -0400

    put all 3 categories into single dic and dump as json

--- data/aggregate_dist_time.py
+++ data/aggregate_dist_time.py
@@ -1,10 +1,9 @@
 # aggregate distance, time for different groups
 
 import csv
+import json
 
 all_dic = {"ALL": {"count": 0, "dist": 0, "time": 0}}
-mode_dic = {}
-aff_dic = {}
 
 if __name__ == "__main__":
     ind_reader = csv.reader(open("mit-commuter-data-time.csv"))
@@ -25,25 +24,27 @@ if __name__ == "__main__":
         all_dic["ALL"]["time"] += time * 2
 
         # put in mode dic
-        if mode not in mode_dic:
-            mode_dic[mode] = {
+        if mode not in all_dic:
+            all_dic[mode] = {
                     "count": count,
                     "dist": dist_mul_cnt * 2,
                     "time": time * 2,
                     }
         else:
-            mode_dic[mode]["count"] += count
-            mode_dic[mode]["dist"] += dist_mul_cnt * 2
-            mode_dic[mode]["time"] += time * 2
+            all_dic[mode]["count"] += count
+            all_dic[mode]["dist"] += dist_mul_cnt * 2
+            all_dic[mode]["time"] += time * 2
 
         # put in aff dic
-        if aff not in aff_dic:
-            aff_dic[aff] = {
+        if aff not in all_dic:
+            all_dic[aff] = {
                     "count": count,
                     "dist": dist_mul_cnt * 2,
                     "time": time * 2,
                     }
         else:
-            aff_dic[aff]["count"] += count
-            aff_dic[aff]["dist"] += dist_mul_cnt * 2
-            aff_dic[aff]["time"] += time * 2
+            all_dic[aff]["count"] += count
+            all_dic[aff]["dist"] += dist_mul_cnt * 2
+            all_dic[aff]["time"] += time * 2
+
+    print json.dumps(all_dic)
